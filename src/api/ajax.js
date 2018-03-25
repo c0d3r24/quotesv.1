618d5923faa842ff42f47e6870ad717d79e46952
Junaid Azhar Shaikh <junaidazharshaikh@gmail.com>
2018-03-25 08:22:54 -0400

    added fetch method object

--- src/api/ajax.js
+++ src/api/ajax.js
@@ -3,9 +3,13 @@ const apiHost   = 'https://tychbees.com/quote_api/';
 export default {
     async fetchAllCategories(){
         try {
-            const response = await fetch(apiHost + 'categories');
+            const response = await fetch(apiHost + 'categories',{
+                method: 'GET',
+                headers: {
+                  Accept: 'application/json',
+                  'Content-Type': 'application/json',
+                }});
             const responseJson = await response.json();
-            console.log(responseJson);
             return responseJson;
         }catch(error){
             console.error(error);
@@ -13,7 +17,12 @@ export default {
     },
     async fetchQuotesForCategory(id){
         try {
-            const response = await fetch(apiHost + 'categories/' + id);
+            const response = await fetch(apiHost + 'categories/' + id,{
+                method: 'GET',
+                headers: {
+                  Accept: 'application/json',
+                  'Content-Type': 'application/json',
+                }});
             const responseJson = await response.json();
             return responseJson;
         }catch(error){
